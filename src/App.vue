b3de74aea50658b197ca269b3f502612a08844d3
Queue-Evgenii <genadolgii228@gmail.com>
2023-01-17 12:08:53 +0200

    main-commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   </div>
 </template>
 <style lang="scss">
-@import url(../src/assets/css/null.css);
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;400&display=swap');
 ._container{
